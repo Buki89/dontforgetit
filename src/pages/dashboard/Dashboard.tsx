96585b34743cc256adc48e3f358e2a8c470aa144
Ondřej Bukartyk <ondrejbukartyk@pop-os.localdomain>
2020-12-11 23:28:20 +0100

    Date picker, gitignore

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,37 +1,57 @@
-import React,{FC,useCallback,useState} from 'react'
-import Header from '../../layout/components/Header';
-import { TaskMock } from '../../Mock/TaskMock';
-import Task,{ TaskProps } from './Task';
-import Input from '../../primitives/components/Input'
+import React, { FC, useCallback, useState } from "react";
+import Header from "../../layout/components/Header";
+import { TaskProps } from "./Task";
+import Input from "../../primitives/components/Input";
+import DatePicker from "react-datepicker";
+import Task from './Task'
+import { v4 as uuidv4 } from 'uuid';
+
+
+ 
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const Dashboard: FC = () => {
-    const [value,setValue] = useState<TaskProps>();
-    const [input,setInput] = useState('');
+  const [value, setValue] = useState<TaskProps[]>([]);
+  const [input, setInput] = useState("");
+  const [date, setDate] = useState<Date  | null | undefined>(new Date());
 
-    const handleOnChange = useCallback((event)=> {
-        const {value} = event.target;
-        setInput(value);
-    },[]);
-    const handleSubmit = useCallback((e)=>{
-        e.preventDefault(); 
-        setValue({id:'as',taskName:input, completed:true, deadline:'20.12:2020'})
-    }
-        ,[input])
+  const handleOnChange = useCallback((event) => {
+    const { value } = event.target;
+    setInput(value);
+  }, []);
 
-console.log(value)
-console.log(input)
 
-    return(
-        <>
-        <Header />
-        <form onSubmit={handleSubmit}>
-        <Input onChange={handleOnChange} value={input}/>
-        <button type='submit'>Add</button>
-        </form>
-    
-        </>
-    )
-}
+
+  const handleDate = useCallback((event)=> setDate(event),[])
+
+  const handleSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      setValue([...value,{
+        id: uuidv4()	,
+        taskName: input,
+        completed: false,
+        deadline: date?.toLocaleDateString(),
+      }]);
+    },
+    [input, value, date], 
+  );
+
+  return (
+    <>
+      <Header />
+      <form onSubmit={handleSubmit}>
+        <Input onChange={handleOnChange} value={input} />
+        <DatePicker dateFormat='dd/MM/yyyy'  selected={date} onChange={handleDate} /> 
+        <button type="submit">Add</button>
+      </form>
+      {value.length !== 0 && value.map((item: TaskProps)=>(
+      <Task key={item.id} id={item.id} taskName={item.taskName} completed={item.completed} deadline={item.deadline}  />
+        
+      ))}
+    </>
+  );
+};
 
 export default Dashboard;
--- src/pages/dashboard/Task.tsx
+++ src/pages/dashboard/Task.tsx
@@ -1,25 +1,27 @@
-import React,{FC} from 'react'
-import styled from 'styled-components'
+import React,{FC,useCallback,useState} from 'react'
 
 
 export type TaskProps = {
     id:string;
     taskName: string;
     completed: boolean;
-    deadline?: string;
+    deadline?: Date | null | string;
 
 
 
 }
 
 const Task: FC<TaskProps> = ({taskName, completed, id, deadline}) => {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = useCallback(()=>setChecked(!checked) ,[checked])
     return(
         <>
     <div>{taskName}</div>
     <button>delete</button>
     <button>edit</button>
     <div>{deadline}</div>
-    <input checked={completed} type='checkbox'></input>
+    <input onChange={handleChange} checked={checked} type='checkbox'></input>
 
     </>
     )
